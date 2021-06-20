e7c2faad5b4c340c16dc937e194a5d2316c26b47
GustavoDare <gustborgdare@gmail.com>
2021-06-20 18:07:37 -0300

    Adicionando robô
    Foi adicionado o robô sem os motores e o controlador em C usado no tutorial do webots. O robô utiliza o controlador para se deslocar para frente, demostrando a sua funcionalidade.

---
+++ Controllers/four_wheeled_collision_avoidance/build/release/four_wheeled_collision_avoidance.d
@@ -0,0 +1,7 @@
+build/release/four_wheeled_collision_avoidance.o: \
+ four_wheeled_collision_avoidance.c \
+ C:\Users\gustb\AppData\Local\Programs\Webots/include/controller/c/webots/distance_sensor.h \
+ C:\Users\gustb\AppData\Local\Programs\Webots/include/controller/c/webots/types.h \
+ C:\Users\gustb\AppData\Local\Programs\Webots/include/controller/c/webots/motor.h \
+ C:\Users\gustb\AppData\Local\Programs\Webots/include/controller/c/webots/robot.h \
+ C:\Users\gustb\AppData\Local\Programs\Webots/include/controller/c/webots/nodes.h
